8d70507a09845df1d9f2126a296e2fced9726726
Steven Dorsher <sdorsher@gmail.com>
2020-05-29 14:16:41 -0500

    I was made for you

--- yourfile.py
+++ yourfile.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-
+import time
 now = datetime.now()
 
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 print("Still waiting for you...")
+time.sleep(30)
+current_time = now.strftime("%H:%M:%S")
+print("Current Time=", current_time)
+print("These stories don't mean anything without someone to tell them to")
